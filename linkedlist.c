943b8fbe369042f698f3c00cd7638d05a842d0d6
Nickhil Sethi <ns1635@nyu.edu>
2021-05-09 14:10:39 -0400

    kr format

--- linkedlist.c
+++ linkedlist.c
@@ -1,62 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int	NUM_NODES = 100;
+const int NUM_NODES = 100;
 
 typedef struct Node {
-	int		value;
-	struct Node    *next;
+    int value;
+    struct Node *next;
 };
 
-struct Node    *
-allocateNode(int i)
+struct Node *allocateNode(int i)
 {
-	struct Node    *node;
-	node = (struct Node *)malloc(sizeof(struct Node));
-	node->value = i;
-	return node;
+    struct Node *node;
+    node = (struct Node *) malloc(sizeof(struct Node));
+    node->value = i;
+    return node;
 };
 
 
-void
-printNode(struct Node *node)
+void printNode(struct Node *node)
 {
-	printf("Node{value: %d, next: %d}\n", node->value, (int)node->next);
+    printf("Node{value: %d, next: %d}\n", node->value, (int) node->next);
 }
 
-void
-printNodes(struct Node *head)
+void printNodes(struct Node *head)
 {
-	int		listExhausted = 0;
-	while (!listExhausted) {
-		printNode(head);
-		if (head->next != NULL) {
-			head = head->next;
-		} else {
-			listExhausted = 1;
-		}
+    int listExhausted = 0;
+    while (!listExhausted) {
+	printNode(head);
+	if (head->next != NULL) {
+	    head = head->next;
+	} else {
+	    listExhausted = 1;
 	}
-	return;
+    }
+    return;
 };
 
-int
-main()
+int main()
 {
 
-	struct Node	head = {
-		.value = 0,
-		.next = NULL
-	};
+    struct Node head = {
+	.value = 0,
+	.next = NULL
+    };
 
-	struct Node    *headPtr = &head;
-	struct Node    *current = &head;
+    struct Node *headPtr = &head;
+    struct Node *current = &head;
 
-	for (int i = 1; i < NUM_NODES; i++) {
-		struct Node    *next = allocateNode(i);
-		current->next = next;
-		current = next;
-	}
+    for (int i = 1; i < NUM_NODES; i++) {
+	struct Node *next = allocateNode(i);
+	current->next = next;
+	current = next;
+    }
 
-	printNodes(headPtr);
-	return 0;
+    printNodes(headPtr);
+    return 0;
 }
