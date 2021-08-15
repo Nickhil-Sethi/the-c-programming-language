45d7793562dde09670a77e96a1033c3310e31f04
Nickhil Sethi <ns1635@nyu.edu>
2021-08-15 11:00:48 -0400

    get longest line program

---
+++ chapter_1/get_longest_line.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* max line length */
+
+int getinputline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+  int len;
+  int max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while ((len = getinputline(line, MAXLINE)) > 0){
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    };
+  };
+  if (max  > 0) {
+    printf("%s", longest);
+  }
+};
+
+int getinputline(char s[], int lim) {
+  char c;
+  int i;
+
+  for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  };
+
+  s[i] = '\0';
+  return i;
+};
+
+void copy(char to[], char from[]) {
+  int i;
+  for (i = 0; from[i] != '\0'; i++){
+    to[i] = from[i];
+  };
+  to[i] = '\0';
+};
